7fe21b04aeb0da3ccdc3906f49b7cd3d0bc320fd
Marcel <marcel@maece.ch>
2025-04-03 21:38:29 +0200

    feat: add yuh import

--- apps/api/src/app/transactions/statementMapper/statment-mapper.factory.ts
+++ apps/api/src/app/transactions/statementMapper/statment-mapper.factory.ts
@@ -4,11 +4,13 @@ import {
   SwisscardStatementMapper,
 } from './swisscard-statement.mapper';
 import { WiseStatementMapper } from './wise-statement.mapper';
+import { YuhStatementMapper } from './yuh-statment.mapper';
 
 type StatementMapper =
   | SwisscardStatementMapper
   | PostFinanceStatementMapper
-  | WiseStatementMapper;
+  | WiseStatementMapper
+  | YuhStatementMapper;
 export type Statement = SwisscardStatement;
 
 export class StatementMapperFactory {
@@ -29,6 +31,9 @@ export class StatementMapperFactory {
     }
     if (statementIssuer === 'WISE') {
       return new WiseStatementMapper(fileContent, accountId);
+    }
+    if (statementIssuer === 'YUH') {
+      return new YuhStatementMapper(fileContent, accountId);
     }
     throw new Error('Invalid statement issuer');
   }
---
+++ apps/api/src/app/transactions/statementMapper/yuh-statment.mapper.ts
@@ -0,0 +1,72 @@
+import { MappedTransaction, StatementMapper } from './base-statement.mapper';
+import { parse } from 'csv-parse/sync';
+
+export interface YuhStatement {
+  DATE: string;
+  DEBIT: number;
+  CREDIT: number;
+  'ACTIVITY NAME': string;
+}
+
+interface YuhStatementResponse extends YuhStatement {
+  issuer: 'YUH';
+}
+
+export class YuhStatementMapper extends StatementMapper<YuhStatement> {
+  protected async parseStatement(): Promise<YuhStatement[]> {
+    return await parse(this.fileContent, {
+      cast: true,
+      columns: true,
+      skip_empty_lines: true,
+      bom: true,
+      delimiter: ';',
+    });
+  }
+
+  protected mapStatement(): Promise<MappedTransaction<YuhStatementResponse>[]> {
+    const mappedStatement: MappedTransaction<YuhStatementResponse>[] = [];
+    console.log(this.statement);
+    for (const transaction of this.statement) {
+      const dmy = transaction.DATE.split('/');
+
+      mappedStatement.push({
+        creditAmount: transaction.CREDIT
+          ? Math.abs(transaction.CREDIT)
+          : Math.abs(transaction.DEBIT),
+        debitAmount: transaction.CREDIT
+          ? Math.abs(transaction.CREDIT)
+          : Math.abs(transaction.DEBIT),
+        description: transaction['ACTIVITY NAME'].substring(
+          1,
+          transaction['ACTIVITY NAME'].length - 1
+        ),
+        transactionDate: new Date(
+          parseInt(dmy[2]),
+          parseInt(dmy[1]) - 1,
+          parseInt(dmy[0])
+        ),
+        raw: {
+          ...transaction,
+          issuer: 'YUH',
+        },
+      });
+    }
+
+    return Promise.resolve(mappedStatement);
+  }
+
+  protected getCreditAccountIdFromStatement(
+    transaction: MappedTransaction<YuhStatementResponse>
+  ): string {
+    if (transaction.raw.CREDIT) {
+      return this.accountId;
+    }
+  }
+  protected getDebitAccountIdFromStatement(
+    transaction: MappedTransaction<YuhStatementResponse>
+  ): string {
+    if (transaction.raw.DEBIT) {
+      return this.accountId;
+    }
+  }
+}
